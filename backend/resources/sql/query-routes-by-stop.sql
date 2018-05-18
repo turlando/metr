f9a016f9e8fdb8679b0ff95933f18addd8adbf17
Tancredi Orlando <tancredi.orlando@gmail.com>
2018-05-18 10:41:16 +0200

    Improve query get routes

--- backend/resources/sql/query-routes-by-stop.sql
+++ backend/resources/sql/query-routes-by-stop.sql
@@ -1,11 +1,6 @@
--- May lord have mercy for my sinful soul
--- that produced such a query.
-SELECT name
+SELECT DISTINCT route.code, route.name, route.type, trip.destination
 FROM route
-WHERE id = (
-    SELECT DISTINCT route_id
-    FROM trip
-    WHERE id IN (
-        SELECT DISTINCT trip_id
-        FROM timetable
-        WHERE stop_id = ?))
+JOIN trip ON trip.route_id = route.id
+JOIN timetable ON timetable.trip_id = trip.id
+JOIN stop ON stop.id = timetable.stop_id
+WHERE stop.id = ?
