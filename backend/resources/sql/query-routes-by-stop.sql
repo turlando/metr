5e0ea0fe6cbc24f520789ba69b7809111056d4f6
Tancredi Orlando <tancredi.orlando@gmail.com>
2018-05-18 21:49:52 +0200

    Fix SQL indentation

--- backend/resources/sql/query-routes-by-stop.sql
+++ backend/resources/sql/query-routes-by-stop.sql
@@ -1,6 +1,17 @@
-SELECT DISTINCT route.code, route.name, route.type, trip.destination
+SELECT DISTINCT
+    stop.code AS stop_code,
+    stop.name AS stop_name,
+    stop.latitude AS stop_latitude,
+    stop.longitude AS stop_longitude,
+    route.code AS route_code,
+    route.name AS route_name,
+    route.type AS route_type,
+    trip.destination AS trip_destination,
+    timetable.time AS timetable_time
 FROM route
 JOIN trip ON trip.route_id = route.id
 JOIN timetable ON timetable.trip_id = trip.id
 JOIN stop ON stop.id = timetable.stop_id
-WHERE stop.id = ?
+WHERE
+    stop.latitude > ? AND stop.latitude < ? AND
+    stop.longitude > ? AND stop.longitude < ?
