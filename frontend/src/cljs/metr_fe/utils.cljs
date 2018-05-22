1a8ff3673ec9914da5fefa11422f9ca992a698fc
Tancredi Orlando <tancredi.orlando@gmail.com>
2018-05-22 10:02:18 +0200

    Remove dead code

--- frontend/src/cljs/metr_fe/utils.cljs
+++ frontend/src/cljs/metr_fe/utils.cljs
@@ -2,14 +2,6 @@
   (:require [goog.string :as gstring]
             [goog.string.format]))
 
-(defn coordinates->rect [[lat lon] delta]
-  (let [delta_lat_in_deg (* delta (/ 1 110.574))
-        delta_lon_in_deg (* delta (/ 1 (* 111.320 (Math/cos lat))))
-        half_lat_delta   (/ delta_lat_in_deg 2)
-        half_lon_delta   (/ delta_lon_in_deg 2)]
-    [(- lat half_lat_delta) (+ lat half_lat_delta)
-     (- lon half_lon_delta) (+ lon half_lon_delta)]))
-
 (defn now []
   (let [d (js/Date.)]
     (gstring/format "%02d:%02d:%02d"
