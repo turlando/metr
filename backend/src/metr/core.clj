fa0d0b5265a8ff37750480623fbcbf6c15e66c85
Tancredi Orlando <tancredi.orlando@gmail.com>
2018-10-30 16:21:35 +0100

    Make server start and data import not mandatory

--- backend/src/metr/core.clj
+++ backend/src/metr/core.clj
@@ -8,32 +8,38 @@
 (defn stop!
   [{:keys [db-conn http-server]
     :as   args}]
-  (server/stop! http-server)
+  (when-not (= http-server :none)
+    (server/stop! http-server))
   (-> db-conn :connection .close)
   nil)
 
 (defn start!
   ([] (start! {}))
-  ([{:keys [db-path]
-     :or   {db-path :memory}
+  ([{:keys [db-path start-http-server? import-data?]
+     :or   {db-path            :memory
+            start-http-server? true
+            import-data?       true}
      :as   args}]
    (let [db-conn     (if (= db-path :memory)
                        (db/get-in-memory-connection)
                        (db/get-file-connection db-path))
-         http-server (server/start! {:db-conn db-conn})
+         http-server (if start-http-server?
+                       (server/start! {:db-conn db-conn})
+                       :none)
          dataset     (gtfs/build-dataset)]
-     (try
-       (jdbc/with-db-transaction [tx db-conn]
-         (db/init-schema! tx)
-         (db/insert-stops! tx (-> dataset :stops))
-         (db/insert-routes! tx (-> dataset :routes))
-         (db/insert-shape-points! tx (-> dataset :shape-points))
-         (db/insert-trips! tx (-> dataset :trips))
-         (db/insert-stop-times! tx (-> dataset :stop-times)))
-       (catch Exception e
-         (stop! {:db-conn     db-conn
-                 :http-server http-server})
-         (throw e)))
+     (when import-data?
+       (try
+         (jdbc/with-db-transaction [tx db-conn]
+           (db/init-schema! tx)
+           (db/insert-stops! tx (-> dataset :stops))
+           (db/insert-routes! tx (-> dataset :routes))
+           (db/insert-shape-points! tx (-> dataset :shape-points))
+           (db/insert-trips! tx (-> dataset :trips))
+           (db/insert-stop-times! tx (-> dataset :stop-times)))
+         (catch Exception e
+           (stop! {:db-conn     db-conn
+                   :http-server http-server})
+           (throw e))))
      {:db-conn     db-conn
       :http-server http-server})))
 
