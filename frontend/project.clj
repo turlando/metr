7c6e88ec25d901167b230eda97b6aac062a6bae8
Tancredi Orlando <tancredi.orlando@gmail.com>
2018-11-06 07:27:29 +0100

    Refactor frontend project.clj

--- frontend/project.clj
+++ frontend/project.clj
@@ -3,7 +3,7 @@
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :min-lein-version "2.7.1"
+  :min-lein-version "2.8.0"
   :plugins [[lein-cljsbuild "1.1.7"]
             [deraen/lein-less4j "0.6.2"]]
 
@@ -19,47 +19,51 @@
                                     "resources/public/js"
                                     "resources/public/css"]
 
+  :cljsbuild
+  {:builds
+   [{:id           "dev"
+     :source-paths ["src/cljs"]
+     :compiler     {:main                 metr-fe.core
+                    :closure-defines      {goog.DEBUG true}
+                    :optimizations        :none
+                    :output-to            "resources/public/js/app.js"
+                    :output-dir           "resources/public/js/out"
+                    :asset-path           "js/out"
+                    :source-map-timestamp true
+                    :preloads             [devtools.preload]
+                    :external-config      {:devtools/config {:features-to-install :all}}}
+     :figwheel     {:websocket-url "ws://[[server-hostname]]:[[server-port]]/figwheel-ws"
+                    :on-jsload     "metr-fe.core/mount-root"}}
+
+    {:id           "min"
+     :source-paths ["src/cljs"]
+     :compiler     {:main            metr-fe.core
+                    :closure-defines {goog.DEBUG false}
+                    :output-to       "resources/public/js/app.js"
+                    :optimizations   :advanced
+                    :pretty-print    false}}]}
+
   :less {:source-paths ["src/less"]
          :target-path  "resources/public/css"}
 
-  :repl-options {:nrepl-middleware [cider.piggieback/wrap-cljs-repl]}
-  :figwheel {:server-port   8081
-             :css-dirs      ["resources/public/css"]}
-
   :profiles
-  {:dev  {:plugins      [[lein-figwheel "0.5.17"]
-                         [lein-doo "0.1.10"]]
-          :dependencies [[binaryage/devtools "0.9.10"]
-                         [figwheel-sidecar "0.5.17"]
-                         [cider/piggieback "0.3.10"]]
-          :less         {:source-map true}}
-   :prod {}}
+  {:dev {:plugins      [[lein-figwheel "0.5.17"]
+                        [lein-doo "0.1.10"]]
+         :dependencies [[binaryage/devtools "0.9.10"]
+                        [figwheel-sidecar "0.5.17"]
+                        [cider/piggieback "0.3.10"]]
+         :source-paths ["src/clj/dev"]
+         :repl-options {:nrepl-middleware [cider.piggieback/wrap-cljs-repl]}
+         :less         {:source-map true}
+         :figwheel     {:server-port 8081
+                        :css-dirs    ["resources/public/css"]}}}
 
-  :aliases {"dev"   ["with-profile" "+dev" "do"
-                     ["clean"]
-                     ["less4j" "once"]
-                     ["figwheel" "dev"]]
-            "build" ["with-profile" "+prod,-dev" "do"
-                     ["clean"]
-                     ["less4j" "once"]
-                     ["cljsbuild" "once" "min"]]}
-
-  :cljsbuild {:builds
-              [{:id           "dev"
-                :source-paths ["src/cljs"]
-                :figwheel     {:websocket-url "ws://[[server-hostname]]:[[server-port]]/figwheel-ws"
-                               :on-jsload "metr-fe.core/mount-root"}
-                :compiler     {:main                 metr-fe.core
-                               :output-to            "resources/public/js/compiled/app.js"
-                               :output-dir           "resources/public/js/compiled/out"
-                               :asset-path           "js/compiled/out"
-                               :source-map-timestamp true
-                               :preloads             [devtools.preload]
-                               :external-config      {:devtools/config {:features-to-install :all}}}}
-               {:id           "min"
-                :source-paths ["src/cljs"]
-                :compiler     {:main            metr-fe.core
-                               :output-to       "resources/public/js/compiled/app.js"
-                               :optimizations   :advanced
-                               :closure-defines {goog.DEBUG false}
-                               :pretty-print    false}}]})
+  :aliases
+  {"dev"   ["with-profile" "+dev" "do"
+            ["clean"]
+            ["less4j" "once"]
+            ["figwheel" "dev"]]
+   "build" ["with-profile" "-dev" "do"
+            ["clean"]
+            ["less4j" "once"]
+            ["cljsbuild" "once" "min"]]})
